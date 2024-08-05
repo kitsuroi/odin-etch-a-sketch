462a42cef940e06d15fa17f63a10cd3260cc03c9
kitsuroi <154440204+kitsuroi@users.noreply.github.com>
2024-08-05 17:57:54 +0200

    feat(create-grid); add create new grid on button press

--- script.js
+++ script.js
@@ -1,31 +1,52 @@
 // Get the container element
 const grid = document.querySelector(".grid-container")
 
-// Create a grid 16 x 16
-let rows =  16;
-let columns= 16;
+//Get the create-button element
+const createButton = document.querySelector(".create-button");
 
-// Loop to create grid items and append them to the container
-for (let i = 0; i < rows; i++) {
-  const gridRow = document.createElement("div");
-  gridRow.classList.add("grid-row");
+// Get user input on click event create button
+createButton.addEventListener("click", () => {
+  getGridSize();
+})
 
-  for (let j = 0; j < columns; j++) {
-    const gridItem = document.createElement("div");
-    gridItem.classList.add("grid-item");
-        
-    // Append the grid item to the container;
-    gridRow.appendChild(gridItem);
+function getGridSize(){
+  gridSize = prompt("Input grid size(maximum 100):");
+  if (gridSize > 100){
+   alert("Please input a number below 101");
+   getGridSize();
+  }else if (gridSize != null){
+   grid.replaceChildren();
+   createGrid(gridSize);
   }
-  grid.appendChild(gridRow);
 }
 
-// Get all grid items
-const gridItems = document.querySelectorAll(".grid-item");
-
-// Set grid items to active when hovered
-gridItems.forEach((gridItem) => {
-  gridItem.addEventListener("mouseover", (event) => {
-  event.target.classList.add("active");
+// Create grid
+function createGrid(gridSize){
+  let rows =  gridSize;
+  let columns= gridSize;
+  
+  // Loop to create grid items and append them to the container
+  for (let i = 0; i < rows; i++) {
+    const gridRow = document.createElement("div");
+    gridRow.classList.add("grid-row");
+    
+    for (let j = 0; j < columns; j++) {
+      const gridItem = document.createElement("div");
+      gridItem.classList.add("grid-item");
+      
+      // Append the grid item to the container;
+      gridRow.appendChild(gridItem);
+    }
+    grid.appendChild(gridRow);
+  }
+  const gridItems = document.querySelectorAll(".grid-item");
+  
+  // Set grid items to active when hovered
+  gridItems.forEach((gridItem) => {
+    gridItem.addEventListener("mouseover", (event) => {
+    event.target.classList.add("active");
+    })
   })
-})
+}
+
+createGrid(16);
