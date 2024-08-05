cf4d20edd423d613f792654481a175c7229ac3e6
kitsuroi <154440204+kitsuroi@users.noreply.github.com>
2024-08-05 14:36:20 +0200

    fix(grid): fix the sizing of grid

--- script.js
+++ script.js
@@ -7,12 +7,20 @@ let columns= 16;
 
 // Loop to create grid items and append them to the container
 for (let i = 0; i < rows; i++) {
+  const gridRow = document.createElement("div");
+  gridRow.classList.add("grid-row");
+
   for (let j = 0; j < columns; j++) {
     const gridItem = document.createElement("div");
     gridItem.classList.add("grid-item");
-    gridItem.textContent = j + 1;
+    // Add a number to grid-item
+    const gridText = document.createElement("div");
+    gridText.classList.add("grid-number");
+    gridText.textContent = j + 1;
+    gridItem.appendChild(gridText);
 
     // Append the grid item to the container;
-    grid.appendChild(gridItem);
+    gridRow.appendChild(gridItem);
   }
+  grid.appendChild(gridRow);
 }
