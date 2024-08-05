32d2cb954e1c291dfc6a6041a28b9518bb3c0d11
kitsuroi <154440204+kitsuroi@users.noreply.github.com>
2024-08-05 15:17:11 +0200

    del(gridNumbers)

--- script.js
+++ script.js
@@ -13,13 +13,7 @@ for (let i = 0; i < rows; i++) {
   for (let j = 0; j < columns; j++) {
     const gridItem = document.createElement("div");
     gridItem.classList.add("grid-item");
-    
-    // Add a number to grid-item
-    /*const gridText = document.createElement("div");
-    gridText.classList.add("grid-number");
-    gridText.textContent = j + 1;
-    gridItem.appendChild(gridText);*/
-    
+        
     // Append the grid item to the container;
     gridRow.appendChild(gridItem);
   }
